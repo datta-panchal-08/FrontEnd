72de9a5a52d02e37a931071c96fdd44025f30839
dattatray panchal <panchaldatta1508@gmail.com>
2024-03-14 08:43:41 +0530

    gsap timeline

--- GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP - Getting Started With Basics/script.js
+++ GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP - Getting Started With Basics/script.js
@@ -7,10 +7,39 @@
 // });  // starting se end tak jane ke liye  initial se final tak
 
 // final se intial position tak le jane ke liye gsap.from use karte hai.
-gsap.from("#box",{
-    x:1000,
-    rotate:360,
+// gsap.from("#box",{
+//     x:1000,
+//     rotate:360,
+//     duration:2,
+//     delay:1,
+//     backgroundColor:"yellow"
+// });
+
+// Timeline
+
+var tl = gsap.timeline();
+
+tl.to("#box1",{
+    x:1200,
+    scale:0.5,
     duration:2,
     delay:1,
-    backgroundColor:"yellow"
+    backgroundColor:"orangered",
+    rotate:360,
 });
+tl.to("#box2",{
+    x:1200,
+    scale:0.5,
+    duration:2,
+    delay:2,
+    backgroundColor:"purple",
+    rotate:360,
+});
+tl.to("#box3",{
+    x:1200,
+    scale:0.5,
+    duration:2,
+    delay:1,
+    backgroundColor:"yellow",
+    rotate:360,
+})
