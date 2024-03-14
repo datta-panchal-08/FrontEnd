a328ad77709a0c876ff92ec8e8f2da0b1d37eab5
dattatray panchal <panchaldatta1508@gmail.com>
2024-03-14 08:13:24 +0530

    basics of gsap

---
+++ GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP - Getting Started With Basics/script.js
@@ -0,0 +1,16 @@
+// gsap.to("#box",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     backgroundColor:"blue",
+//     rotate:360
+// });  // starting se end tak jane ke liye  initial se final tak
+
+// final se intial position tak le jane ke liye gsap.from use karte hai.
+gsap.from("#box",{
+    x:1000,
+    rotate:360,
+    duration:2,
+    delay:1,
+    backgroundColor:"yellow"
+});
