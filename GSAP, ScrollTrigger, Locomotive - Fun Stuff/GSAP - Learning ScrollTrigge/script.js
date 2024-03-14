3004f8d807498051dda04f0d3da2dceb1cc13c2c
dattatray panchal <panchaldatta1508@gmail.com>
2024-03-14 12:22:59 +0530

    learned ScrollTrigger In GSAP

---
+++ GSAP, ScrollTrigger, Locomotive - Fun Stuff/GSAP - Learning ScrollTrigge/script.js
@@ -0,0 +1,21 @@
+gsap.from("#page1 #circle",{
+     scale:0,
+     delay:1,
+     duaration:2,
+     rotate:720
+});
+gsap.from("#page2 #circle",{
+    scale:0,
+    delay:1,
+    duaration:2,
+    rotate:720,
+    scrollTrigger:{
+        trigger:"#page2 #circle",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        end:"top 30%",
+        scrub:2
+
+    }
+});
