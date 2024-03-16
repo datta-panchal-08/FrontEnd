b87e4287808d353791db588a4a1d6f3f2c1a84d8
dattatray panchal <panchaldatta1508@gmail.com>
2024-03-16 14:48:31 +0530

    learned How To Reuse Components In React

--- React Js/Section 3 Reusable Components/src/App.jsx
+++ React Js/Section 3 Reusable Components/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import Card from './Components/Card'
 const App = () => {
+  const arr =[
+    {name:"Datta",age:22},
+    {name:"Sonali",age:20},
+    {name:"Ganesh",age:23},
+    
+  ]
   return (
     <div>
+       <Card/>
       
     </div>
   )
--- React Js/Section 3 Reusable Components/src/Components/Card.jsx
+++ React Js/Section 3 Reusable Components/src/Components/Card.jsx
@@ -1,19 +1,66 @@
-import React from 'react'
+import React from "react";
 
 const Card = () => {
-  return (
-   <div className='w-full h-screen bg-zinc-200'>
-    <div className='w-52 bg-zinc-100 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] rounded-md overflow-hidden'>
-      <div className='w-full h-32 bg-zinc-300 '>
-        <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1633174524778-61a18ee54490?q=80&w=1496&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-      </div>
-      <div className='px-3 py-4'>
-        <h1 className='font-bold text-center text-xl tracking-tighter'>Amazon Bsics.</h1>
-        <p className='font-semibold text-sm w-4/4 text-center mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio iure voluptatibus possimus?</p>
-      </div>
-    </div>
-   </div>
-  )
-} 
+  const card = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1633174524778-61a18ee54490?q=80&w=1496&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Amazon Basics",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio iure voluptatibus possimus?",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1493723843671-1d655e66ac1c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Daily Objects",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio iure voluptatibus possimus?",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1654573817889-296cad084c97?q=80&w=1462&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Flipkart Basics",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio iure voluptatibus possimus?",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1544244015-0df4b3ffc6b0?q=80&w=1415&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Ipad",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio iure voluptatibus possimus?",
+    },
+  ];
 
+  return (
+    <div className="w-full flex  items-center justify-center gap-10 h-screen bg-zinc-200">
+      {card.map((elem, index) => {
+        return (
+          <>
+            <div
+              key={index}
+              className="w-52  bg-zinc-100 rounded-md overflow-hidden"
+            >
+              <div className="w-full h-32 bg-zinc-300  ">
+                <img
+                  className="w-full h-full object-cover"
+                  src={elem.image}
+                  alt=""
+                />
+              </div>
+              <div className="px-3 py-4">
+                <h1 className="font-bold text-center text-xl tracking-tighter">
+                  {elem.name}
+                </h1>
+                <p className="font-semibold text-sm w-4/4 text-center mt-3">
+                  {elem.description}
+                </p>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
+};
 export default Card;
