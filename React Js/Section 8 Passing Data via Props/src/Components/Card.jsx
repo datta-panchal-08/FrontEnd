97c471af73175105f5e48259834b3bf8196d9655
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-17 18:34:45 +0530

    Passing Data Via Props Part 2

--- React Js/Section 8 Passing Data via Props/src/Components/Card.jsx
+++ React Js/Section 8 Passing Data via Props/src/Components/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({values,handleClick,index}) => {
         <div className='w-full p-3'>
             <h3 className='text-xl font-semibold'>{name}</h3>
             <h5 className=' text-xs'>{profession}</h5>
-            <button onClick={()=>handleClick(index)} className={`px-3 py-1 mt-4 rounded-md ${friends === true ? "bg-green-500" : "bg-red-600"} font-xs text-white font-semibold`}>{friends === true ? "Friends":"Add Friend"}</button>
+            <button onClick={()=>handleClick(index)} className={`px-3 py-1 mt-4 rounded-md ${friends ? "bg-green-500" : "bg-red-600"} font-xs text-white font-semibold`}>{friends === true ? "Friends":"Add Friend"}</button>
         </div>
     </div>
   )
