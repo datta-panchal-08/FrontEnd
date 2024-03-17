6f69208f90414df3bb2aa3bc60a92237d79a0667
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-03-17 21:45:02 +0530

    Successfully Build Add To Favorites Feature

--- React Js/Section 9 Props and State Exercise/src/Components/Card.jsx
+++ React Js/Section 9 Props and State Exercise/src/Components/Card.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({data,handleClick,index}) => {
+    const {song,artist,image,added} = data;
   return (
-    <div>Card</div>
+    <div className='relative w-60 rounded-md pb-8 bg-white flex justify-center gap-3 p-4 mt-10 '>
+        <div className='w-20 h-20 bg-orange-500 rounded-md overflow-hidden'>
+            <img className='w-full h-full  object-cover object-center' src={image} alt="" />
+        </div>
+        <div>
+        <h1 className='text-sm font-semibold whitespace-nowrap'>{song}</h1>
+        <h2 className='text-xs'>{artist}</h2>
+        </div>
+        <button onClick={()=>handleClick(index)} className={`px-4 absolute py-2 bottom-[-10%] left-1/2 whitespace-nowrap -translate-x-[50%] rounded-full text-xs text-white  ${added === true ? "bg-green-500":"bg-orange-500"} `}>{added === false ? "Add To Favorites" :"Added"}</button>
+    </div>
   )
 }
 
--- React Js/Section 9 Props and State Exercise/src/Components/Navbar.jsx
+++ React Js/Section 9 Props and State Exercise/src/Components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({data}) => {
   return (
-    <div>Navbar</div>
+    <div className='w-full px-3 py-4 flex items-center justify-between'>
+          <h3>Orange</h3>
+          <div className='bg-orange-500 px-4 py-2 flex items-center justify-center gap-2 rounded-md'>
+            <h4 className='text-sm text-white font-semibold '>Favorites</h4>
+            <h5 className='text-xs text-white'>{data.filter(item=>item.added).length}</h5>
+
+          </div>
+
+    </div>
   )
 }
 
